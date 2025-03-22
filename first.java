0f0c68cb75c3897ac1bb80944e27af196ac7be6d
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-03-22 11:31:44 +0530

    Java class

---
+++ first.java
@@ -0,0 +1,11 @@
+// output on java 
+
+public class first {
+     public static void main(String args[]){ //String => S should be capital
+        System.out.println("hello");  //System => S should be capital   
+
+     }
+}
+
+
+
---
+++ input.java
@@ -0,0 +1,20 @@
+// input in java from user 
+
+
+import java.util.Scanner;
+public class input {
+   public static void main (String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the first number");
+         int a = sc.nextInt();
+         System.out.println("Enter the second number");
+         int b = sc.nextInt();
+         int c = a+b;
+         System.out.println(c);
+         System.out.println(a);
+         System.out.println("a");
+      sc.close();
+   }
+
+    
+}
