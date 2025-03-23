d5c2c25ded54b2a31c3eada3f95e78d777c14492
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-03-23 13:19:30 +0530

    OOPS

--- OOPS.java
+++ OOPS.java
@@ -1,17 +1,30 @@
 public class OOPS{
-    
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // created a pen object
+        p1.setColor("Red");
+        System.out.println(p1.color);
+        p1.setTip(10);
+        System.out.println(p1.tip);
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
 }
 
 class Pen {
     String color;
     int tip;
     
-
-    void setColor(int newTip ){
-         tip = newTip;
+    void setTip(int newTip) {
+        tip = newTip;
     }
-    
+
     void setColor(String newColor){
         color = newColor;
     }
+}
+
+class Student {
+    String name;
+    int age;
+    Pen pen;
 }
