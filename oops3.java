69f68515b786e96808e64abba096a0392c723c49
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-03-30 12:44:08 +0530

    done

---
+++ oops3.java
@@ -0,0 +1,39 @@
+public class oops3 {
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // created a pen object
+        p1.setColor("Red");
+        System.out.println(p1.color);
+        p1.setTip(10);
+        System.out.println(p1.tip);
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+    
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    Pen pen;
+}
+//     Student(String name, int age, Pen pen) {
+
+//         this.name = name;
+//         this.age = age;
+
+//         this.pen = pen;
+//     }
+//     void printInfo() {
+
